f8760f4dd0088a24968bcde011c05f8aeb730038
Atif.Ali <atif.ali@nmxglobalsoftware.com>
2023-07-07 17:18:01 +0500

    dockerfile added

---
+++ app/Http/Controllers/Hbv2UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Hbv2UserController extends Controller
+{
+    public function getUser($id)
+    {
+        // Fetch the user from the database based on the $id
+        // ...
+
+        // Return the response
+        return response()->json(['id' => $id, 'name' => 'John Doe', 'email' => 'john@example.com']);
+    }
+
+    public function createUser(Request $request)
+    {
+        // Perform validation and create the user
+        // ...
+
+        // Return the response
+        return response()->json(['message' => 'User created successfully']);
+    }
+
+    public function updateUser(Request $request, $id)
+    {
+        // Perform validation and update the user based on the $id
+        // ...
+
+        // Return the response
+        return response()->json(['message' => 'User updated successfully']);
+    }
+
+    public function deleteUser($id)
+    {
+        // Delete the user from the database based on the $id
+        // ...
+
+        // Return the response
+        return response()->json(['message' => 'User deleted successfully']);
+    }
+}
